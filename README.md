18054f71874ac6478aa9ef2b7dc3a99c83eca869
Chris <890popbox@users.noreply.github.com>
2020-02-16 02:20:41 -0800

    zzz

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sea-Guide
+
+Short script for new shit dungeon, using OWYN's nest as a boilerplate.
+
+Call out attacks from Sea of Honour
+
+*Script is shit, will collect more data to fix
+
+Please report any issues to me either here or on discord :)
+
+# Commands 
+- !soh to toggle module (DEFAULT: ON)
+- !soh toparty to toggle party notice (DEFAULT: SELF ONLY (In notice chat))
+- !soh toparty stream - to toggle messages being sent only to proxy channel, also disables itemhelp (DEFAULT: OFF)
+- !soh itemhelp - turns off\on placing signs on safe spots for right\left swings (DEFAULT: ON)
+
+# Attacks announced
+
+Onle last boss, almost everything, send IDs that are being forgotten.
---
+++ index.js
@@ -0,0 +1,160 @@
+/* Usable Sysbols ◎●←↑→↓↖↗↘↙ */
+
+const mapID = [3020];	    // MAP ID to input [ Normal de , Hard de ] 
+
+//Only boss that matters
+const obtm = {
+	108: {msg: 'Basic / Front Stun'},
+	120: {msg: '**Combo Charging**'},
+	121: {msg: '**Combo Charging**'},
+	122: {msg: '**Combo Charging**'},
+	123: {msg: '**Combo Charging**'},
+	124: {msg: 'Front Hammer Slam (Out Safe)'},
+	125: {msg: 'Spin (In Safe)'},
+	126: {msg: 'Big Front Hammer Slam (Out Safe)'},
+	127: {msg: 'Jump'},
+	128: {msg: 'Upper cut ~Knock up'},
+	129: {msg: 'Hammer Toss'},
+	131: {msg: 'UNK'},
+	132: {msg: 'UNK'},
+	133: {msg: '2ndary Aggro Jump > Outward Donuts'},
+	134: {msg: 'Big Spin (In Safe)'},
+	135: {msg: 'Puddle Inc.'},
+	136: {msg: '??? UNK'},
+	137: {msg: '??? In > Out'},
+	138: {msg: '??? In > Out Projectile'},
+	139: {msg: '??? Out > In'},
+	201: {msg: '??? UNK 30%?'},
+	202: {msg: '202 Hold Block? 3 Seconds'},
+	203: {msg: 'Jump over puddles'},
+	204: {msg: '??? UNK?'},
+};
+
+//Export the module
+module.exports = function soh_guide(d) {
+	let command = d.command;
+	let hooks = [],
+		bossCurLocation,
+		bossCurAngle,
+		uid0 = 999999999n,
+		sendToParty = false,
+		enabled = true,
+		itemhelper = true,
+	   	streamenabled = false;
+	
+	//Load the map
+	d.hook('S_LOAD_TOPO', 3, (event) => {
+		if (event.zone === mapID[0]) 
+		{								
+			command.message('Welcome to SOH');
+			load();
+		} 
+		else
+		{
+			unload();
+		}
+    });
+	
+	//Turn it on and off
+	command.add(['soh', '!soh'], {
+        $none() {
+            enabled = !enabled;
+			command.message('Sea of Honour '+(enabled ? 'Enabled' : 'dabled') + '.');
+		},
+		$default() {
+			command.message('Error (typo?) in command! see README for the list of valid commands')
+		},
+		itemhelp() {
+			itemhelper = !itemhelper;
+			command.message('Item helper for safe spots '+(itemhelper ? 'Enabled' : 'dabled') + '.');
+		},
+		toparty(arg) {
+			if(arg === "streamsoh")
+			{
+				streamenabled = !streamenabled;
+				sendToParty = false;
+				itemhelper = false;
+				command.message((streamenabled ? 'Stream de Enabled' : 'Stream de dabled'));
+			}
+			else
+			{
+				streamenabled = false;
+				sendToParty = !sendToParty;
+				command.message((sendToParty ? 'Sea of Honour Guide - Messages will be sent to the party' : 'Nest Guide - Only you will see messages in chat'));
+			}
+		}
+	});
+	
+	function sendMessage(msg)
+	{
+		if (sendToParty) 
+		{
+			d.toServer('C_CHAT', 1, {
+			channel: 21, //21 = p-notice, 1 = party, 2 = guild
+			message: msg
+			});
+		}
+		else if(streamenabled) 
+		{
+			command.message(msg);
+		}
+		else 
+		{
+			d.toClient('S_CHAT', 3, {
+			channel: 21, //21 = p-notice, 1 = party
+			name: 'DG-Guide',
+			message: msg
+			});
+		}
+	}
+	
+	function load()
+	{
+		if(!hooks.length)
+		{
+			hook('S_ACTION_STAGE', 9, (event) => {
+				if(!enabled || event.stage != 0) return;
+				
+				if (event.templateId === 1000)
+				{
+					let skill = event.skill.id % 1000;
+					if(obtm[skill])
+					{
+						sendMessage(obtm[skill].msg);
+					}
+				}
+				else if (event.templateId === 2000)
+				{
+					let skill = event.skill.id % 1000;
+					if(obtm[skill])
+					{
+						sendMessage(obtm[skill].msg);
+					}
+				}
+				else if (event.templateId === 2200)
+				{
+					let skill = event.skill.id % 1000;
+					if(obtm[skill])
+					{
+						sendMessage(obtm[skill].msg);
+					}
+				}
+			});
+		}
+	}
+	
+	function unload()
+	{
+		if(hooks.length)
+		{
+			for(let h of hooks) d.unhook(h);
+
+			hooks = []
+		}
+	}
+
+	function hook()
+	{
+		hooks.push(d.hook(...arguments));
+	}
+}
